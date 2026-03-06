577b541845205db540c72db306c608bf9e6fab04
ampliorcloud-glitch <ampliorcloud-glitch@users.noreply.github.com>
2026-03-06 12:53:23 +0530

    feat: redirect naked domain to www

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,21 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'altleads.com',
+          },
+        ],
+        destination: 'https://www.altleads.com/:path*',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
